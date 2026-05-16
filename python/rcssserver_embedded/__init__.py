be446313e6734ac79f148677a01c016b80e16f5d
MinghanWu <miw039@ucsd.edu>
2026-05-16 03:15:46 -0700

    Added back the python init

---
+++ python/rcssserver_embedded/__init__.py
@@ -0,0 +1,19 @@
+from ._core import BallState
+from ._core import EmbeddedSimulator
+from ._core import GameState
+from ._core import PlayMode
+from ._core import PlayerCommand
+from ._core import PlayerState
+from ._core import Side
+from ._core import Vector2
+
+__all__ = [
+    "BallState",
+    "EmbeddedSimulator",
+    "GameState",
+    "PlayMode",
+    "PlayerCommand",
+    "PlayerState",
+    "Side",
+    "Vector2",
+]
